a02591016d7c258a90004ce0966da7d1fe65db85
Amisha6572 <kulkarniamisha@gmail.com>
2026-03-18 10:24:09 +0530

    connected to railway db

--- config.py
+++ config.py
@@ -12,12 +12,18 @@ def _get(key: str, default=None):
         return os.getenv(key, default)
 
 DB_CONFIG = {
-    "host":     _get("DB_HOST", "localhost"),
-    "user":     _get("DB_USER", "root"),
-    "password": _get("DB_PASSWORD", ""),
-    "database": _get("DB_NAME", "internal_mobility_db"),
-    "port":     int(_get("DB_PORT", 3306)),
-}
 
+    DB_HOST = "mysql.railway.internal"
+    DB_USER = "root"
+    DB_PASSWORD = "KxUqYodTNQJIjIsSegUemjINzpqQriMT"
+    DB_NAME = "railway"
+    DB_PORT = 33602
+    "host": st.secrets["DB_HOST"],
+    "user": st.secrets["DB_USER"],
+    "password": st.secrets["DB_PASSWORD"],
+    "database": st.secrets["DB_NAME"],
+    "port": st.secrets["DB_PORT"]
+    "auth_plugin": "mysql_native_password"
+}
 APP_NAME = "InternalMobility Hub"
 VERSION  = "1.0.0"
